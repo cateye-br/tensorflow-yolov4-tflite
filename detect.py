98f295070224219c1e3f48aa8a839fee3fe33ac4
leosapucaia <leo.sapucaia@gmail.com>
2022-04-20 11:26:39 -0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -78,9 +78,10 @@ def main(_argv):
         iou_threshold=FLAGS.iou,
         score_threshold=FLAGS.score
     )
-    pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
 
-    print(pred_bbox)
+    print(boxes, scores)
+
+    pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
 
     image = utils.draw_bbox(original_image, pred_bbox)
     # image = utils.draw_bbox(image_data*255, pred_bbox)
